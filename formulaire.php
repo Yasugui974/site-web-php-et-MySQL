a5abd4ac1c087e364bc3b6421eddd53d9a135fc3
Yasugui974 <guillaumepavade@hotmail.fr>
2019-02-11 14:25:35 +0400

    php

--- formulaire.php
+++ formulaire.php
@@ -3,13 +3,22 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Page mot de passe NASA</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
 </head>
 <body>
-    
+<p>
+Veuillez entrer le mot de passe!
+</p>  
+<form method="post" action="secret.php" >
+<p>
+<input type="password" name="password" />
+<input type="submit" value="Valider" />
+</p>
+
+</form>
 
 
 
@@ -17,6 +26,13 @@
 
 
 
-    
+
+
+
+
+
+<script type="text/javascript">
+alert('Le FBI a vos coordonner veuillez ne pas vous tromper')
+</script> 
 </body>
 </html>
--- secret.php
+++ secret.php
@@ -3,18 +3,36 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Page code des bombes</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
 </head>
 <body>
-    
+
+
+<?php
+if ($_POST['password'] == ('kangourou'))
+{
+    echo 'bonjour monsieur le Président'.'<br/>';
+    echo '<br> voici tout les codes confidentiel dont vous avez besoin';
+    echo '<br> 08359982379497375909 bombes nucléaire sur tout les continents';
+    echo '<br> 50646086081068864841 bombes IEM sur tout les continents'; 
+    echo '<br> 1234567890 apocalipse Zombie, Alien etc...';
+}
+else
+{
+echo 'Error!Error!Error'.'<br/>';
+echo '<br> Envoie de drones anti-personnel sur adresse IP en cour';
+echo '<br> Extermination de cible potentiel et prioritère';
+}
+?>
 
 
 
+ 
 
 
-    
+ 
 </body>
 </html>
